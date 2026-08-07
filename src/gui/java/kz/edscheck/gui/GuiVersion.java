dcedbc53be8a8655b61bc0f2e3eda53c9c689f30
vturekhanov <vladimir@turekhanov.org>
2026-08-07 14:45:34 +0500

    Version bump

--- src/gui/java/kz/edscheck/gui/GuiVersion.java
+++ src/gui/java/kz/edscheck/gui/GuiVersion.java
@@ -1,7 +1,7 @@
 package kz.edscheck.gui;
 
 public final class GuiVersion {
-    public static final String VALUE = "1.1.0";
+    public static final String VALUE = "1.1.1";
 
     private GuiVersion() {
     }
